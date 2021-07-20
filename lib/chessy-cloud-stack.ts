0bab815a70f6a25f761797b4b55b277b92aee3df
Ivan Slobodiuk <ivanslo@yahoo.com.ar>
2021-07-20 09:54:58 +0100

    Create SQS Queues

--- lib/chessy-cloud-stack.ts
+++ lib/chessy-cloud-stack.ts
@@ -1,10 +1,12 @@
 import * as cdk from "@aws-cdk/core";
 import * as s3 from "@aws-cdk/aws-s3";
 import * as dynamodb from "@aws-cdk/aws-dynamodb";
+import * as sqs from "@aws-cdk/aws-sqs";
 
 import { BlockPublicAccess } from "@aws-cdk/aws-s3";
 import { RemovalPolicy } from "@aws-cdk/core";
 import { AttributeType, BillingMode } from "@aws-cdk/aws-dynamodb";
+import { Duration } from "@aws-cdk/aws-dynamodb/node_modules/@aws-cdk/core";
 
 export class ChessyCloudStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -36,5 +38,23 @@ export class ChessyCloudStack extends cdk.Stack {
         removalPolicy: RemovalPolicy.DESTROY,
       });
     }
+
+    /* SQS Queues
+    --------------------*/
+    const deadLetterQueue = new sqs.Queue(this, "chessy-pgn-games-failed-dlq", {
+      queueName: "chessy-pgn-games-failed-dlq",
+
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    new sqs.Queue(this, "chessy-pgn-games-to-process", {
+      queueName: "chessy-pgn-games-to-process",
+      visibilityTimeout: Duration.minutes(2),
+      deadLetterQueue: {
+        maxReceiveCount: 2,
+        queue: deadLetterQueue,
+      },
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
   }
 }
