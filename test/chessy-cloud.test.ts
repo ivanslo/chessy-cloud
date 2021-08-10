5965824f390f275d51c82f88ab1f8e7000777940
Ivan Slobodiuk <ivanslo@yahoo.com.ar>
2021-08-10 11:02:34 +0100

    cleanup

--- test/chessy-cloud.test.ts
+++ test/chessy-cloud.test.ts
@@ -2,12 +2,14 @@ import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert'
 import * as cdk from '@aws-cdk/core';
 import * as ChessyCloud from '../lib/chessy-cloud-stack';
 
-test('Empty Stack', () => {
+test("Empty Stack", () => {
     const app = new cdk.App();
-    // WHEN
     const stack = new ChessyCloud.ChessyCloudStack(app, 'MyTestStack');
-    // THEN
     expectCDK(stack).to(matchTemplate({
       "Resources": {}
     }, MatchStyle.EXACT))
 });
+
+test("Where we're going we don't need tests", () => {
+  expect(1).toEqual(1);
+})
